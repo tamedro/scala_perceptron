f9a5e47994dac0aa69cf9fc5eb7142a646dc62f3
tamedro <3feet4d5>
2017-04-13 23:34:39 -0600

    :hammer: sign and dot

--- src/main/scala/Perceptron.scala
+++ src/main/scala/Perceptron.scala
@@ -1,9 +1,19 @@
 object Perceptron extends App {
-  printArray()
+  val w_0 = (0d, 0d)
+  val x_0 = (-2d, 2d)
+  val x = List((-2d, 2d))
 
-  def printArray(): Unit = {
-    val l = List(1, 2, 3, 4)
-    l.toStream.foreach(print)
+  println(sign(dot(w_0, x_0)))
+
+  def sign(x: Double): Int = {
+    if (x >= 0d) 1 else -1
   }
 
+  def dot(x_1: (Double, Double), x_2: (Double, Double)): Double = {
+    (x_1._1 * x_2._1) + (x_1._2 + x_2._2)
+  }
+
+  def printArray(xs: List[(Double, Double)]): Unit = {
+    xs.toStream.foreach(print)
+  }
 }
